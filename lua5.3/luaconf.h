254682228554121920565b228e2a4afa1ae4f36b
Chunhui He <hchunhui@mail.ustc.edu.cn>
2019-07-15 12:49:32 +0000

    add multiarch support

--- lua5.3/luaconf.h
+++ lua5.3/luaconf.h
@@ -201,14 +201,26 @@
 #else			/* }{ */
 
 #define LUA_ROOT	"/usr/local/"
+#define LUA_ROOT2	"/usr/"
 #define LUA_LDIR	LUA_ROOT "share/lua/" LUA_VDIR "/"
+#define LUA_LDIR2	LUA_ROOT2 "share/lua/" LUA_VDIR "/"
 #define LUA_CDIR	LUA_ROOT "lib/lua/" LUA_VDIR "/"
+#define LUA_CDIR3	LUA_ROOT2 "lib/lua/" LUA_VDIR "/"
+
 #define LUA_PATH_DEFAULT  \
 		LUA_LDIR"?.lua;"  LUA_LDIR"?/init.lua;" \
 		LUA_CDIR"?.lua;"  LUA_CDIR"?/init.lua;" \
+		LUA_LDIR2"?.lua;"  LUA_LDIR2"?/init.lua;" \
 		"./?.lua;" "./?/init.lua"
+#ifdef LUA_MULTIARCH
+#define LUA_CDIR2	LUA_ROOT2 "lib/" LUA_MULTIARCH "/lua/" LUA_VDIR "/"
 #define LUA_CPATH_DEFAULT \
-		LUA_CDIR"?.so;" LUA_CDIR"loadall.so;" "./?.so"
+		LUA_CDIR"?.so;" LUA_CDIR2"?.so;" LUA_CDIR3"?.so;" LUA_CDIR"loadall.so;" "./?.so"
+#else
+#define LUA_CPATH_DEFAULT \
+		LUA_CDIR"?.so;" LUA_CDIR3"?.so;" LUA_CDIR"loadall.so;" "./?.so"
+#endif
+
 #endif			/* } */
 
 
