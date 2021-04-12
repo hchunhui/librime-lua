7bf6c2c20c7101e6cb9f808f6c0fd4950730fb76
shewer <shewer@gmail.com>
2021-04-13 03:01:19 +0800

    update sample
    Signed-off-by: shewer <shewer@gmail.com>

--- sample/lua/projection.lua
+++ sample/lua/projection.lua
@@ -5,19 +5,40 @@
 
 
 
+local function test_code(env,pfl2,preedit_fmt_list)
+	print("==== test get_int bool double string =====")
+	local b=env.engine.schema.config:get_bool(  "translator/enable_encoder")
+	local i=env.engine.schema.config:get_int(   "menu/page_size")
+	local d=env.engine.schema.config:get_double("translator/initial_quality")
+	local s=env.engine.schema.config:get_string("translator/tips")
+	print( "bool", b , 
+		   "int" , i ,
+		   "double", d,
+		   "string", s)
+
+	-- print value 
+	print(" ConfigList from translator/preedit")
+	for i=0,preedit_fmt_list.size-1 do 
+		print(i,  preedit_fmt_list:get_value_at(i).value)
+	end 
+
+	-- print value 
+	print(" ConfigList from user")
+	for i=0,pfl2.size-1 do 
+		print(i,  preedit_fmt_list:get_value_at(i).value)
+	end 
+	--  test projection  :  p1   p2
+	local str1= "abcdefg"
+	print("porjection test :" , str1 , env.p1:apply(str1), env.p2:apply(str1) )
+	--  
+end 
 
 local function init_func(env)
-
-	local config=env.engine.schema.config
 	-- load ConfigList  form  schema path
-	local proedit_fmt_list = conifg:get_list("translastor/preedit_format")
-	-- print value 
-	for i=0,proedit_fmt_list.size do 
-		print(i,  proedit_fmt_list:get_value_at(i).value)
-	end 
+	local preedit_fmt_list = env.engine.schema.config:get_list("translator/preedit_format")
 	--   create Projection obj
 	local p1=Projection()
-	p1:load(proedit_fmt_list)
+	p1:load(preedit_fmt_list)
 
 	--  user create  Projection obj
 	--  create ConfigList obj 
@@ -28,19 +49,19 @@ local function init_func(env)
 	pfl2:append( ConfigValue("xlit|def|tuv|").element )  -- element referce to (ConfigItem *)  
 	pfl2:insert(1,ConfigValue("xlit|ghi|qrs|").element )
 
+
 	--   create Projection obj
 	local p2=Projection()
 	p2:load(pfl2)
-	p2:append("xlit|ijk|opq|") 
-	local str1= "abcdefg"
-	--   
-	print(str1 , p1:apply(str1), p2:apply(str1) )
-	--  
 	env.p1=p1
 	env.p2=p2
+    -- 
+    test_code( env, pfl2, preedit_fmt_list )
+
 end 
 local function func(input,seg,env)
 	for cand in input:iter() do 
+		-- reprace cand.comment 
 		cand.comment=  env.p1(cand.comment) .. env.p2(cand.comment)
 		yield(cand)
 	end
