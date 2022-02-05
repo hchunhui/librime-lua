aea5d236bb60bcd37d18e663a5f11e392cac017c
Shewer Lu <shewer@gmail.com>
2022-02-06 00:35:23 +0800

    improve error messages for raw_init
    Signed-off-by: Shewer Lu <shewer@gmail.com>

--- src/lua_gears.cc
+++ src/lua_gears.cc
@@ -41,7 +41,11 @@ static void raw_init(lua_State *L, const Ticket &t,
       int status = lua_pcall(L, 1, 1, 0);
       if (status != LUA_OK) {
         const char *e = lua_tostring(L, -1);
-        LOG(ERROR) << "Lua Compoment of " << t.name_space << " initialize  error:(" << status << "): " << e;
+        LOG(ERROR) << "Lua Compoment of initialize  error:("
+          << " module: "<< t.klass
+          << " name_space: " << t.name_space
+          << " status: " << status
+          << " ): " << e;
       }
     }
     lua_pop(L, 1);
@@ -55,6 +59,14 @@ static void raw_init(lua_State *L, const Ticket &t,
     lua_getfield(L, -1, "func");
   }
 
+  if (lua_type(L, -1) != LUA_TFUNCTION) {
+    LOG(ERROR) << "Lua Compoment of " << t.name_space << " initialize error:( func_ type: " << luaL_typename(L, -1) << " expect function )";
+    LOG(ERROR) << "Lua Compoment of initialize  error:("
+      << " module: "<< t.klass
+      << " name_space: " << t.name_space
+      << " func type: " << luaL_typename(L, -1)
+      << " ): " << "func type error expect function ";
+  }
   *func = LuaObj::todata(L, -1);
   lua_pop(L, 1);
 }
