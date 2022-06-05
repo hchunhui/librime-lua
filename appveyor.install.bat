9fe9f1e80d83b4df0a740eed60b6b5a50c9ca68d
居戎氏 <chen.sst@gmail.com>
2022-06-05 12:53:42 +0800

    chore: remove obsolete config, scripts for travis CI and appveyor

--- appveyor.install.bat
+++
@@ -1,1 +0,0 @@
-git clone https://github.com/hchunhui/librime-lua.git -b thirdparty --depth=1 %plugin_dir%\thirdparty
--- appveyor.rime-install.bat
+++
@@ -1,5 +0,0 @@
-set plugin_dir=plugins\librime-lua
-mklink /j \projects\librime\%plugin_dir% .
-cd \projects\librime
-call %plugin_dir%\appveyor.install.bat
-.\appveyor.install.bat
