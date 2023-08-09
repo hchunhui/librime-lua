8ef08a2c296000f1dd2663f65e0073f8f5a25ca6
shewer <shewer@gmail.com>
2023-08-09 20:52:28 +0800

    refactor raw_init()
    Signed-off-by: shewer <shewer@gmail.com>

--- src/lua_gears.cc
+++ src/lua_gears.cc
@@ -2,6 +2,13 @@
 #include "lua_gears.h"
 
 namespace rime {
+#define LUA_TANY -2
+
+struct InitTab {
+  string name;
+  an<LuaObj>* luaobj;
+  int type;//lua type : LUA_TFUNCTION ...
+};
 
 //--- LuaTranslation
 bool LuaTranslation::Next() {
@@ -20,10 +27,8 @@ bool LuaTranslation::Next() {
     return true;
   }
 }
-
-//---
-static void raw_init(lua_State *L, const Ticket &t,
-                     an<LuaObj> *env, an<LuaObj> *func, an<LuaObj> *fini, an<LuaObj> *tags_match= NULL) {
+//(-0,+1,+m)
+static void raw_init_env(lua_State *L, const Ticket &t, an<LuaObj> *env) {
   lua_newtable(L);
   Engine *e = t.engine;
   LuaType<Engine *>::pushdata(L, e);
@@ -32,7 +37,10 @@ static void raw_init(lua_State *L, const Ticket &t,
   lua_setfield(L, -2, "name_space");
   *env = LuaObj::todata(L, -1);
   lua_pop(L, 1);
+}
 
+//(-0, +1) table
+static bool raw_load_module(lua_State *L, const Ticket &t) {
   if (t.klass.size() > 0 && t.klass[0] == '*') {
     lua_getglobal(L, "require");
     lua_pushstring(L, t.klass.c_str() + 1);
@@ -49,55 +57,79 @@ static void raw_init(lua_State *L, const Ticket &t,
     lua_getglobal(L, t.klass.c_str());
   }
 
-  if (lua_type(L, -1) == LUA_TTABLE) {
-    lua_getfield(L, -1, "init");
-    if (lua_type(L, -1) == LUA_TFUNCTION) {
-      LuaObj::pushdata(L, *env);
-      int status = lua_pcall(L, 1, 1, 0);
-      if (status != LUA_OK) {
-        const char *e = lua_tostring(L, -1);
-        LOG(ERROR) << "Lua Compoment of initialize  error:("
-          << " module: "<< t.klass
-          << " name_space: " << t.name_space
-          << " status: " << status
-          << " ): " << e;
-      }
-    }
-    lua_pop(L, 1);
-
-    lua_getfield(L, -1, "fini");
-    if (lua_type(L, -1) == LUA_TFUNCTION) {
-      *fini = LuaObj::todata(L, -1);
-    }
-    lua_pop(L, 1);
-    
-    if (tags_match) {
-      lua_getfield(L, -1, "tags_match");
-      if (lua_type(L, -1) == LUA_TFUNCTION) {
-        *tags_match = LuaObj::todata(L, -1);
-      }
-      lua_pop(L, 1);
-    }
-
-    lua_getfield(L, -1, "func");
+  int ltype = lua_type(L, -1);
+  if ( ltype == LUA_TTABLE) {
+    return true;
+  } else if ( ltype == LUA_TFUNCTION) {
+    lua_newtable(L);
+    lua_rotate(L, -2, 1);
+    lua_setfield(L, -2, "func");
+    return true;
+  } else {
+    LOG(ERROR) << "loading module [" << t.klass << "@" << t.name_space << "]"
+	       << luaL_typename(L, -1)
+	       << " error:() type error function or table expected ";
+    lua_newtable(L);
+    lua_rotate(L, -2, 1);
+    lua_setfield(L, -2, "func"); // LUA_TANY
+    return false;
   }
-
-  if (lua_type(L, -1) != LUA_TFUNCTION) {
-    LOG(ERROR) << "Lua Compoment of initialize  error:("
-      << " module: "<< t.klass
-      << " name_space: " << t.name_space
-      << " func type: " << luaL_typename(L, -1)
-      << " ): " << "func type error expect function ";
-  }
-  *func = LuaObj::todata(L, -1);
-  lua_pop(L, 1);
 }
+// (-0, +0, +m) regist InitTab.luaobj
+static void reg_funcs(lua_State *L, int index, InitTab *luaobjs, size_t size) {
+  for (int i=0; i < size; i++) {
+    auto t = luaobjs[i];
+    int ltype = lua_getfield(L, index, t.name.c_str());
+    if ( t.type == LUA_TANY || t.type == ltype) {
+      *(t.luaobj) = LuaObj::todata(L, -1);
+    }
+    lua_pop(L, 1);
+  }
+}
+//---
+// (-0, +0, +m) update luaobjs
+static void raw_init(lua_State *L, const Ticket &t,
+                     an<LuaObj> *env, InitTab *luaobjs, size_t size) {
+  raw_init_env(L, t, env);//(-0, +0, +m) update env
+  raw_load_module(L, t);//(-0, +1)  table
+  reg_funcs(L, -1, luaobjs, size);//(-0,+0,+m)  luaobjs
+  lua_pop(L, 1); // remove module table
+}
+  // ex: VOID_CALL(init_, "init")
+#define VOID_CALL(func, str_fname)					\
+  do {									\
+    if (func) {								\
+      auto r = lua_->void_call<an<LuaObj>, an<LuaObj>>(func, env_);	\
+      if (!r.ok()) {							\
+	auto e = r.get_err();						\
+	LOG(ERROR) << typeid(*this).name() << "::" << __FUNCTION__	\
+		   << " [" << name_space_ << ": " #str_fname " ] "	\
+		   << " error(" << e.status << "): " << e.e;		\
+      }									\
+    }									\
+  } while(0)
+
+#define F_FIELD(name) { #name, &name##_, LUA_TFUNCTION }
+#define A_FIELD(name) { #name, &name##_, LUA_TANY }
+
+#define DEF_FIELD()				\
+  {"init", &init, LUA_TFUNCTION },		\
+    F_FIELD(fini),				\
+    A_FIELD(func)
+
+
 
 //--- LuaFilter
 LuaFilter::LuaFilter(const Ticket& ticket, Lua* lua)
   : Filter(ticket), TagMatching(ticket), lua_(lua) {
-  lua->to_state([&](lua_State *L) {raw_init(L, ticket, &env_, &func_, &fini_, &tags_match_);});
+  an<LuaObj> init;
+  InitTab luaobjs[]= { DEF_FIELD(), F_FIELD(tags_match), };
+  size_t size = sizeof(luaobjs) / sizeof(InitTab);
+
+  lua->to_state([&](lua_State *L) {raw_init(L, ticket, &env_, luaobjs, size );});
+  VOID_CALL(init, "init");
 }
+
 
 an<Translation> LuaFilter::Apply(
   an<Translation> translation, CandidateList* candidates) {
@@ -106,20 +138,17 @@ an<Translation> LuaFilter::Apply(
   return New<LuaTranslation>(lua_, f);
 }
 
-LuaFilter::~LuaFilter() {
-  if (fini_) {
-    auto r = lua_->void_call<an<LuaObj>, an<LuaObj>>(fini_, env_);
-    if (!r.ok()) {
-      auto e = r.get_err();
-      LOG(ERROR) << "LuaFilter::~LuaFilter of "<< name_space_ << " error(" << e.status << "): " << e.e;
-    }
-  }
-}
+LuaFilter::~LuaFilter() { VOID_CALL(fini_, "fini"); }
 
 //--- LuaTranslator
 LuaTranslator::LuaTranslator(const Ticket& ticket, Lua* lua)
   : Translator(ticket), lua_(lua) {
-  lua->to_state([&](lua_State *L) {raw_init(L, ticket, &env_, &func_, &fini_);});
+  an<LuaObj> init;
+  InitTab luaobjs[]= { DEF_FIELD() };
+  size_t size = sizeof(luaobjs) / sizeof(InitTab);
+
+  lua->to_state([&](lua_State *L) {raw_init(L, ticket, &env_, luaobjs, size);});
+  VOID_CALL(init, "init");
 }
 
 an<Translation> LuaTranslator::Query(const string& input,
@@ -133,20 +162,17 @@ an<Translation> LuaTranslator::Query(const string& input,
     return t;
 }
 
-LuaTranslator::~LuaTranslator() {
-  if (fini_) {
-    auto r = lua_->void_call<an<LuaObj>, an<LuaObj>>(fini_, env_);
-    if (!r.ok()) {
-      auto e = r.get_err();
-      LOG(ERROR) << "LuaTranslator::~LuaTranslator of "<< name_space_ << " error(" << e.status << "): " << e.e;
-    }
-  }
-}
+LuaTranslator::~LuaTranslator() { VOID_CALL( fini_, "fini"); }
 
 //--- LuaSegmentor
 LuaSegmentor::LuaSegmentor(const Ticket& ticket, Lua *lua)
   : Segmentor(ticket), lua_(lua) {
-  lua->to_state([&](lua_State *L) {raw_init(L, ticket, &env_, &func_, &fini_);});
+  an<LuaObj> init;
+  InitTab luaobjs[]= { DEF_FIELD(),};
+  size_t size = sizeof(luaobjs) / sizeof(InitTab);
+
+  lua->to_state([&](lua_State *L) {raw_init(L, ticket, &env_, luaobjs, size);});
+  VOID_CALL(init, "init");
 }
 
 bool LuaSegmentor::Proceed(Segmentation* segmentation) {
@@ -160,20 +186,18 @@ bool LuaSegmentor::Proceed(Segmentation* segmentation) {
     return r.get();
 }
 
-LuaSegmentor::~LuaSegmentor() {
-  if (fini_) {
-    auto r = lua_->void_call<an<LuaObj>, an<LuaObj>>(fini_, env_);
-    if (!r.ok()) {
-      auto e = r.get_err();
-      LOG(ERROR) << "LuaSegmentor::~LuaSegmentor of "<< name_space_ << " error(" << e.status << "): " << e.e;
-    }
-  }
-}
+LuaSegmentor::~LuaSegmentor() { VOID_CALL( fini_, "fini"); }
+
 
 //--- LuaProcessor
 LuaProcessor::LuaProcessor(const Ticket& ticket, Lua* lua)
   : Processor(ticket), lua_(lua) {
-  lua->to_state([&](lua_State *L) {raw_init(L, ticket, &env_, &func_, &fini_);});
+  an<LuaObj> init;
+  InitTab luaobjs[]= { DEF_FIELD() };
+  size_t size = sizeof(luaobjs) / sizeof(InitTab);
+
+  lua->to_state([&](lua_State *L) {raw_init(L, ticket, &env_, luaobjs, size);});
+  VOID_CALL(init, "init");
 }
 
 ProcessResult LuaProcessor::ProcessKeyEvent(const KeyEvent& key_event) {
@@ -191,14 +215,6 @@ ProcessResult LuaProcessor::ProcessKeyEvent(const KeyEvent& key_event) {
     }
 }
 
-LuaProcessor::~LuaProcessor() {
-  if (fini_) {
-    auto r = lua_->void_call<an<LuaObj>, an<LuaObj>>(fini_, env_);
-    if (!r.ok()) {
-      auto e = r.get_err();
-      LOG(ERROR) << "LuaProcessor::~LuaProcessor of "<< name_space_ << " error(" << e.status << "): " << e.e;
-    }
-  }
-}
+LuaProcessor::~LuaProcessor() { VOID_CALL( fini_, "fini"); }
 
 }  // namespace rime
