5cf4a51deaf995c901a948052a4df17ead260c9c
shewer <shewer@gmail.com>
2023-10-25 11:53:21 +0800

    fixed boost::optional error
    Signed-off-by: shewer <shewer@gmail.com>

--- src/lib/luatype_boost_optional.h
+++ src/lib/luatype_boost_optional.h
@@ -1,23 +1,39 @@
 #ifndef LUATYPE_BOOST_OPTIONAL_H
 #define LUATYPE_BOOST_OPTIONAL_H
 
-#include "lua_templates.h"
+#if __cplusplug >= 201703L || _MSVC_VER >=201703L
+#include <optional>
+#else
 #include <boost/optional.hpp>
+#endif // C++17
+       //
+#include "lua_templates.h"
+
+
+
+namespace ns {
+#if __cplusplug >= 201703L || _MSVC_VER >=201703L
+  using std::optional;
+#else
+  using boost::optional;
+#endif// C++17
+
+}// namespace
 
 template<typename T>
-struct LuaType<boost::optional<T>> {
-  static void pushdata(lua_State *L, boost::optional<T> o) {
+struct LuaType<ns::optional<T>> {
+  static void pushdata(lua_State *L, ns::optional<T> o) {
     if (o)
       LuaType<T>::pushdata(L, *o);
     else
       lua_pushnil(L);
   }
 
-  static boost::optional<T> &todata(lua_State *L, int i, C_State *C) {
+  static ns::optional<T> &todata(lua_State *L, int i, C_State *C) {
     if (lua_type(L, i) == LUA_TNIL)
-      return C->alloc<boost::optional<T>>();
+      return C->alloc<ns::optional<T>>();
     else
-      return C->alloc<boost::optional<T>>(LuaType<T>::todata(L, i, C));
+      return C->alloc<ns::optional<T>>(LuaType<T>::todata(L, i, C));
   }
 };
 
